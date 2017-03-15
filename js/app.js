c514a842bfc4fc147cd1ae1e07e6345e5d7fc166
jennifer <jennifer.cruzz@gmail.com>
2017-03-15 14:15:24 -0500

    refactor: Change functions that update body's background image
    Instead of using sprint progress, now the app calls viewModel's functions
    that take into account status in the 3 boards.

--- js/app.js
+++ js/app.js
@@ -233,6 +233,28 @@ var LocationsViewModel = function() {
   this.analysis = new Analysis();
   this.code = new Code();
   // TODO: refactor code isOk... etc, so that param is not passed from UI?
+
+  // TODO: give better names... like 'sprintIsOk' but more generic
+  this.isIdeal = function () {
+      return !this.analysis.failed() && (this.sprint.isExpectedProgress() || this.sprint.isTooEarly()) && this.code.isIdeal();
+  };
+
+  this.isOk = function () {
+      // TODO: When is too early... how should be categorize: ok or ideal?
+      return !this.analysis.failed() &&
+          (this.sprint.isExpectedProgress() || this.sprint.isOKProgress() || this.sprint.isTooEarly()) &&
+          (this.code.isIdeal() || this.code.isOk());
+  };
+
+  this.isBad = function () {
+      return this.analysis.failed() || this.sprint.isBadProgress() || this.code.isBad();
+  };
+
+  this.isCritical = function () {
+      return this.analysis.failed() || this.sprint.isInDangerProgress() || this.code.isInDanger();
+  };
+
+
 };
 
 var viewModel = new LocationsViewModel();
@@ -241,7 +263,6 @@ ko.applyBindings(viewModel);
 viewModel.sprint.update(jsonSprint);
 viewModel.analysis.update(jsonJenkins);
 viewModel.code.update(jsonSonar);
-
 
  // setInterval(function(){
 //    $.get('...', function(){...});
