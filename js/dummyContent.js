6d4aafc24ed7ee4a9c12be2437253c0727b55a0f
jennifer <jennifer.cruzz@gmail.com>
2017-03-17 15:35:42 -0500

    tests: Add additional pointsPerState cases to DummyContent

--- js/dummyContent.js
+++ js/dummyContent.js
@@ -1,15 +1,26 @@
-var t = new Date(Date.now());
-var today = t.getTime();
-var in2weeks = new Date(2017, t.getMonth(), t.getDay() + 15).getTime();
+var secondsInADay = 86400;
+var milisecondsInADay = secondsInADay * 1000;
+
+var today = Date.now();
+var aWeekAgo = today - (milisecondsInADay * 7);
+var aWeekFromNow = today + (milisecondsInADay * 7);
+var twoWeeksFromNow = today + (milisecondsInADay * 14);
+
+var allPointsInFirstStages = '"pointsPerState":{"Ready for QA":0,"To Do":10,"Done":0,"Ready for Sign Off":0,"In Dev":13,"Code Review":5,"In QA":0,"Desk Check":0},';
+var allPointsInLastStages = '"pointsPerState":{"Ready for QA":0,"To Do":0,"Done":8,"Ready for Sign Off":15,"In Dev":0,"Code Review":0,"In QA":5,"Desk Check":0},';
+var allPointsAreWellDistributed = '"pointsPerState":{"Ready for QA":4,"To Do":3,"Done":3,"Ready for Sign Off":3,"In Dev":4,"Code Review":3,"In QA":4,"Desk Check":4},';
+var original = '"pointsPerState":{"Ready for QA":0,"To Do":0,"Done":0,"Ready for Sign Off":0,"In Dev":13,"Code Review":15,"In QA":0,"Desk Check":0},';
+
 // http://localhost:8090/sprint
 var jsonSprint = '{"allStatus":["To Do","In Dev","Code Review","Desk Check","Ready for QA","In QA","Ready for Sign Off","Done"],'+
-                   '"pointsPerState":{"Ready for QA":0,"To Do":0,"Done":0,"Ready for Sign Off":0,"In Dev":13,"Code Review":15,"In QA":0,"Desk Check":0},'+
+                   original +
+                   // allPointsInFirstStages +
+                   // allPointsInLastStages +
+                   // allPointsAreWellDistributed +
                    '"committedPoints":28,'+
                    '"currentSprint":9,'+
-                   '"startDate":' + today + ','+
-                   '"endDate":'+ in2weeks + ','+
-                   // '"startDate":1488261600000,'+
-                   // '"endDate":1489467600000,'+
+                   // '"startDate":' + today + ',"endDate":'+ twoWeeksFromNow + ','+ // sprint starts today
+                   '"startDate":' + aWeekAgo + ',"endDate":'+ aWeekFromNow + ','+ // sprint is half way through
                    '"stories":[{"summary":"[QMO] - APU - Incorrect Log format in Service Alert Logs","status":"Done","complexity":0,"type":"Defect"},'+
                               '{"summary":"Parts Transfer - Login and Logout","status":"In Dev","complexity":8,"type":"Story"},'+
                               '{"summary":"Parts Transfer - Expiration Session ","status":"Code Review","complexity":5,"type":"Story"},'+
--- test/helper.js
+++ test/helper.js
@@ -1,27 +1,3 @@
 
 var secondsInADay = 86400;
 var milisecondsInADay = secondsInADay * 1000;
-
-
-function getInitialTimestamp() {
-    // returns a timestamp corresponding to current date
-    return new Date(Date.now()).getTime();
-}
-
-function getFinalTimestamp(daysFromToday) {
-    // returns a timestamp corresponding to 15 days after current date
-    var today = new Date(Date.now());
-    var todayDay = today.getDate();
-    var todayMonth = today.getMonth();
-
-    var newDay = todayDay + daysFromToday + 1;
-    var newMonth = todayMonth;
-
-    if (todayDay > 15) {
-        var dif = 30 - todayDay;
-        newDay = 15 - dif;
-        newMonth = todayMonth + 1;
-    }
-
-    return new Date(2017, newMonth, newDay).getTime();
-}
